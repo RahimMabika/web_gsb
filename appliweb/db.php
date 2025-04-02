89602f7bf188eb8b17d1ce5f26a5c561a9a613f4
Rahim <rahimmabika@gmail.com>
2025-04-02 15:24:37 +0200

    Delete appliweb directory

--- appliweb/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=frais_app;charset=utf8', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Connexion réussie à la base de données.";
-} catch (Exception $e) {
-    die('❌ Erreur : ' . $e->getMessage());
-}
-?>
--- appliweb/stats.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="style.css">
-<?php
-session_start();
-if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
-    header('Location: login.php');
-    exit;
-}
-echo "<h2>Page des Statistiques</h2>";
-?>
